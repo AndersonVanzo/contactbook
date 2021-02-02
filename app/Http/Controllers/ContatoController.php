05eaf5a6d0ae71b1d002770bd748439d2d904a15
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2021-02-02 00:27:00 -0300

    upload com erro 422

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Contato;
 use Illuminate\Cache\Repository;
+use SebastianBergmann\Environment\Console;
+use Symfony\Component\Console\Input\Input;
 
 class ContatoController extends Controller
 {
@@ -16,14 +18,33 @@ class ContatoController extends Controller
 
     public function store(Request $request)
     {
-        $contato = new Contato([
-            'nome' => $request->input('nome'),
-            'endereco' => $request->input('endereco'),
-            'numero' => $request->input('numero'),
-            'nascimento' => $request->input('nascimento'),
+
+        $request->validate([
+            'foto' => 'required|mimes:jpg,jpeg,png,csv,txt,xlx,xls,pdf|max:2048'
         ]);
 
-        $contato->save();
+        $contato = new Contato;
+        $nome = $request->input('nome');
+        $endereco = $request->input('endereco');
+        $nascimento = $request->input('nascimento');
+
+        if($request->file()) {
+            $file_name = time().'_'.$request->file->getClientOriginalName();
+            $file_path = $request->file('foto')->storeAs('uploads', $file_name, 'public');
+            $contato->nome = $nome;
+            $contato->endereco = $endereco;
+            $contato->nascimento = $nascimento;
+            $contato->foto = `/storage/`.$file_path;
+
+            $contato->save();
+        } else {
+            $contato->nome = $nome;
+            $contato->endereco = $endereco;
+            $contato->nascimento = $nascimento;
+            $contato->foto = '/storage/default-user.png';
+
+            $contato->save();
+        }
 
         return response()->json('Contato salvo!');
     }
